e5b91462ac64a9ea48428d3b36da438f14b4e6c9
mswilliam <m_s_william@yahoo.fr>
2020-06-18 19:42:48 +0200

    add script shell format

---
+++ s04_scriptingStandards/script_shell_format.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Purpose: Testing script format
+# Date: 18/06/2020
+# Modification: 18/06/2020
+
+a='My name is MANGOUA SOFACK William'
+
+echo $a
+
+date
+df -h
+
+  if { $a -eq file}
+  then echo this
+  else
+    echo that
+  fi
